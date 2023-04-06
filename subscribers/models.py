f87fc8379d76a82113975f28fedb98d32902ab8f
naimur <68554905+naimur-naiyimu@users.noreply.github.com>
2023-04-06 19:50:51 +0600

    create subscribers models

--- subscribers/models.py
+++ subscribers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from app.models import Company
 
-# Create your models here.
+class PhoneCompany(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Subscriber(models.Model):
+    phone_number = models.CharField(max_length=20)
+    company = models.ForeignKey(PhoneCompany, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.phone_number
